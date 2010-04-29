910854d83d809016cd0df06d1c71e3d9b89a4916
zkim <zachary.kim@gmail.com>
2010-04-29 00:38:26 -0600

    Updated readme, added license info

---
+++ README.md
@@ -0,0 +1,15 @@
+# leiningen-init-script
+
+A leiningen plugin that allows you to generate *NIX init scripts.
+
+## Usage
+
+Add <code>[leiningen-init-script "0.1.0"]</code> to the :dev-dependencies section in your project.clj.
+
+## Configuration
+
+FIXME: write
+
+## License
+
+[Eclipse Public License v1.0](http://www.eclipse.org/legal/epl-v10.html)
--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject leiningen-init-script "0.1.0"
   :description "A leiningen plugin that allows you to generate *NIX init scripts."
   :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]])
+                 [org.clojure/clojure-contrib "1.1.0"]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
